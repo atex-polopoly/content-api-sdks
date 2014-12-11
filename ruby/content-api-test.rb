e77c8407dddb4ef0ab31dbf06282e999621fe47d
Giulio Mola <gmola@atex.com>
2014-12-11 10:54:47 +0100

    ruby libraries

---
+++ ruby/content-api-test.rb
@@ -0,0 +1,46 @@
+#!/usr/bin/ruby
+require_relative 'content-api'
+require 'test/unit'
+
+class IntegrationTestDataApi < Test::Unit::TestCase
+
+  def setup
+    @api = ContentApi.new
+    @api.user('edmund','edmund').auth
+    @content_id = create_fixture() if @content_id.nil?
+  end
+
+  def create_fixture
+    article_json = <<-ARTICLE
+    {
+      "contentData": {
+        "_type": "com.atex.standard.article.ArticleBean",
+        "title": "Hipster Ipsum",
+        "body": "Butcher mumblecore cred sriracha, four dollar toast chambray fashion axe deep v Godard direct trade selfies Pinterest. Godard quinoa drinking vinegar, fanny pack health goth biodiesel Wes Anderson deep v locavore. Hashtag sriracha hoodie synth McSweeney's scenester, fap yr jean shorts chambray beard. Meggings lomo DIY scenester Etsy, keffiyeh pop-up. Mlkshk disrupt authentic, chillwave plaid cliche wayfarers. Normcore lumbersexual iPhone, Carles art party salvia Austin mlkshk bitters Shoreditch fixie sustainable gluten-free semiotics.",
+        "lead": "Retro cardigan tousled twee, DIY cornhole before they sold out keffiyeh hoodie fap scenester pop-up Carles cray chambray."
+      }
+    }
+    ARTICLE
+    return @api.create(article_json)
+  end
+
+  def test_simple_get
+    data = @api.get @content_id
+    assert_not_nil(data)
+    assert_not_nil(data.etag)
+    assert_equal(data.content_id, @content_id)
+  end
+
+  def test_get_aspect
+    data = @api.get @content_id
+    assert_not_nil(data.aspect('p.InsertionInfo'))
+  end
+
+  def test_upate_from_json
+    data = @api.get @content_id
+    data.aspect('contentData').title = 'made of steel'
+    @api.update(data.content_id, data.etag, data.json.select { |key,_| key =~ /contentData/ })
+  end
+
+
+end
---
+++ ruby/content-api.rb
@@ -0,0 +1,106 @@
+#
+# Client library for the Polopoly Content APi, tested compatible with Polopoly v10.12.
+# Refer to content-api-test.rb for usage examples
+#
+
+require 'rest_client'
+require 'json'
+
+
+class ContentApi
+
+  def initialize
+    @base_url="http://localhost:8080/onecms"
+    @credentials = <<-json
+    {
+      "username": "sysadmin",
+      "password": "sysadmin"
+    }
+    json
+  end
+
+  def base_url(base_url)
+    @base_url = base_url
+    self
+  end
+
+  def user(name, pwd)
+    @credentials = <<-json
+    {
+      "username": "#{name}",
+      "password": "#{pwd}"
+    }
+    json
+    self
+  end
+
+  def auth
+    response = RestClient.post(@base_url + "/security/token",
+    @credentials,
+    :content_type => :json,
+    :accept => :json)
+    token = JSON.parse(response)['token']
+    @headers = {:content_type => :json, :accept => :json, :'X-Auth-Token' => token}
+    self
+  end
+
+  def get(content_id)
+    raw = RestClient.get(@base_url + "/content/contentid/" + content_id, @headers)
+    Content.new raw
+  end
+
+  def create(json)
+    response = RestClient.post(@base_url + "/content", json, @headers)
+    return JSON.parse(response)['id']
+  end
+
+  def update(content_id, etag, json)
+    RestClient.put(@base_url + "/content/contentid/" + content_id, json.to_s, @headers.merge({:'If-Match' => etag}))
+  end
+
+
+  class Content
+
+    def initialize(data); @raw=data; end
+
+    def etag; @raw.headers[:etag]; end
+
+    def aspect(aspect)
+      Aspect.new json()[aspect]
+    end
+
+    def content_id; json()['id']; end
+
+    def json; @json = JSON.parse(@raw) if @json.nil?; @json; end
+
+    def to_s; @raw.to_s; end
+
+  end
+
+
+  class Aspect
+
+    def initialize(json); @json=json; end
+
+    def json; @json; end
+
+    def method_missing(meth, *args, &block)
+      field = meth.to_s.gsub(/=$/,"")
+      unless @json[field].nil?
+        if (meth.to_s.end_with? "=")
+          @json[field] = args[0]
+        else
+          @json[field]
+        end
+      else
+        super
+      end
+    end
+
+    def to_s; @json; end
+
+  end
+
+
+
+end
